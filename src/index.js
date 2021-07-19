20c285471ac2386f82921e676475e73244e47456
Gagneet Singh <gagneet@gmail.com>
2021-07-20 08:31:20 +1000

    Updated to use JSX for teh UL and LI elements with HTML/XML syntax

--- src/index.js
+++ src/index.js
@@ -5,16 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  React.createElement(
-    "ul", 
-    {style: {color: "blue"}}, 
-    React.createElement("li", null, "Monday"),
-    React.createElement("li", null, "Tuesday"),
-    React.createElement("li", null, "Wednesday"),
-    React.createElement("li", null, "Thursday"),
-    React.createElement("li", null, "Friday"),
-    React.createElement("li", null, "Saturday"),
-    React.createElement("li", null, "Sunday")
-  ),
+  <ul>
+    <li>Monday</li>
+    <li>Tuesday</li>
+    <li>Wednesday</li>
+    <li>Thursday</li>
+    <li>Friday</li>
+    <li>Saturday</li>
+    <li>Sunday</li>
+  </ul>,
   document.getElementById('root')
 );
