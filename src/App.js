3de6e33fb2111d8a41fc1cbfb2fdeae3c9c76e9b
Gagneet Singh <gagneet@gmail.com>
2021-07-20 17:56:49 +1000

    Using useReducer with an input checkbox control to show if the checkbox is checked or not

--- src/App.js
+++ src/App.js
@@ -4,33 +4,14 @@ import React, { useState, useEffect } from 'react';
 
 
 function App() {
-  const [emotion, setEmotion] = useState("happy");
-  const [secondary, setSecondary] = useState("tired");
+  const [checked, setChecked] = useState(false);
 
-  useEffect(() => {
-    console.log(`It's ${emotion} around here!`)
-  }, [emotion]);
-
-  useEffect(() => {
-    console.log(`It's ${secondary} around here!`)
-  }, [secondary]);
-
-  console.log(emotion);
   return (
     <>
-      <h1>Hello World! Current emotion is {emotion} and {secondary}.</h1>
-      <button onClick={() => setEmotion("happy")}>
-        Make Happy
-      </button>
-      <button onClick={() => setEmotion("frustrated")}>
-        Frustrated
-      </button>
-      <button onClick={() => setEmotion("angry")}>
-        Angry
-      </button>
-      <button onClick={() => setSecondary("crabby")}>
-        Make Crabby
-      </button>
+      <input type="checkbox" value={checked} onChange={() => setChecked(checked => !checked)} />
+      <p>
+        {checked ? "checked" : "not checked"}
+      </p>
     </>
   )
 }
