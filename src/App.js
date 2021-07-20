f8f17701f41b08b3ed7d95b9de031288b406518e
Gagneet Singh <gagneet@gmail.com>
2021-07-20 18:05:17 +1000

    Using useReducer to toggle and change the state of a element, without useState

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React, { useReducer } from 'react';
 
 
 function App() {
-  const [checked, setChecked] = useState(false);
+  const [checked, toggle] = useReducer(
+    (checked) => !checked,
+    false
+  );
 
   return (
     <>
-      <input type="checkbox" value={checked} onChange={() => setChecked(checked => !checked)} />
+      <input type="checkbox" value={checked} onChange={toggle} />
       <p>
         {checked ? "checked" : "not checked"}
       </p>
     </>
-  )
+  );
 }
 
 export default App;
