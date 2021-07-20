67214714215f7bcf07d6da8bb36e9a2ce15cb6e1
Gagneet Singh <gagneet@gmail.com>
2021-07-20 12:19:42 +1000

    Working with useEffect. Mostly console.log changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 function App() {
   const [emotion, setEmotion] = useState("happy");
+  const [secondary, setSecondary] = useState("tired");
+
+  useEffect(() => {
+    console.log(`It's ${emotion} around here!`)
+  }, [emotion]);
+
+  useEffect(() => {
+    console.log(`It's ${secondary} around here!`)
+  }, [secondary]);
+
   console.log(emotion);
   return (
     <>
-      <h1>Hello World! Current emotion is {emotion}.</h1>
+      <h1>Hello World! Current emotion is {emotion} and {secondary}.</h1>
+      <button onClick={() => setEmotion("happy")}>
+        Make Happy
+      </button>
       <button onClick={() => setEmotion("frustrated")}>
         Frustrated
       </button>
       <button onClick={() => setEmotion("angry")}>
         Angry
+      </button>
+      <button onClick={() => setSecondary("crabby")}>
+        Make Crabby
       </button>
     </>
   )
