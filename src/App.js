ee5cd26b3da3b7c011a234aa976f133daf93d8bb
Gagneet Singh <gagneet@gmail.com>
2021-07-20 19:40:35 +1000

    Updated with error handling and loading states

--- src/App.js
+++ src/App.js
@@ -5,29 +5,37 @@ import React, { useState, useEffect } from 'react';
 
 function App({login}) {
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
+    if (!login) return;
+    setLoading(true);
+
     fetch(`https://api.github.com/users/${login}`)
       .then((response) => response.json())
-      .then(setData);
-  }, [])
+      .then(setData)
+      .then(() => setLoading(false))
+      .catch(setError);
+  }, [login]);
 
-  if(data) {
-    return (
-      <div>
-        <h1>{data.name}</h1>
-        <p>{data.location}</p>
-        <img alt={data.login} src={data.avatar_url} />
-      </div>
-    )
-  }
+  if (loading) return <h1>Loading...</h1>;
+  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
+  if (!data) return null;
 
   return (
-    <>
-      <div>
-        No User Available!
-      </div>
-    </>
+    <div>
+      <h1>{data.name}</h1>
+      <h2>{data.company}</h2>
+      <h3>{data.blog}</h3>
+      <h3>{data.id}</h3>
+      <h3>{data.created_at}</h3>
+      <p>
+        {data.location} <br />
+        {data.bio}
+      </p>
+      <img alt={data.login} src={data.avatar_url} height="200" />
+    </div>
   );
 }
 
