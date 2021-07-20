3fe1c5a5550d2b8f59630e83722d816eb202375c
Gagneet Singh <gagneet@gmail.com>
2021-07-20 10:25:44 +1000

    Using short form for IF-ELSE

--- src/App.js
+++ src/App.js
@@ -16,15 +16,11 @@ function RegularComponent() {
 
 
 function App(props) {
-  if(props.authorized) {
-    return (
-      <SecretComponent />
-    )
-  } else {
-    return (
-      <RegularComponent />
-    );
-  }
+  return (
+    <>
+    {props.authorized ? <SecretComponent /> : <RegularComponent />};
+    </>
+  )
 }
 
 export default App;
