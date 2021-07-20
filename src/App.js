b061f68f80c5fe325c47fe7b58bd4bb03dede7f8
Gagneet Singh <gagneet@gmail.com>
2021-07-20 10:21:12 +1000

    Removed all other components and cleaned up the code

--- src/App.js
+++ src/App.js
@@ -2,47 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
-function Header(props) {
-  return (
-    <header>
-      <h1>First {props.name} App</h1>
-    </header>
-  )
-}
-
-function Main(props) {
-  return (
-    <section>
-      <p> Serving the {props.adjective} web pages over the Internet</p>
-      <img 
-        src={logo} 
-        height={200} 
-        alt="The React Org" 
-      />
-      <img 
-        src="https://github.com/gagneet.png"
-        height={200}
-        alt="GitHub Profile"
-      />
-      <ul style={{textAlign: 'left'}}>
-        {props.dishes.map((dish) => (
-        <li key={dish.id}>
-          {dish.title}
-        </li>
-        ))}
-      </ul>
-    </section>
-  )
-}
-
-function Footer(props) {
-  return (
-    <footer>
-      <p>This goes at the bottom of my page for {props.year}!</p>
-    </footer>
-  )
-}
-
 function SecretComponent() {
   return (
     <h1>Super Secret Information for Authorized Users only</h1>
@@ -55,15 +14,6 @@ function RegularComponent() {
   )
 }
 
-const dishes = [
-  "Macaroni",
-  "Chicken",
-  "Bhindi",
-  "Salmon"
-];
-
-const dishObjects = dishes.map((dish, i) => ({id: i, title: dish}));
-
 
 function App(props) {
   if(props.authorized) {
@@ -71,16 +21,9 @@ function App(props) {
       <SecretComponent />
     )
   } else {
-      return (
-        <React.Fragment>
-          <RegularComponent />
-          <div className="App">
-            <Header name="AdhereLive"/>
-            <Main adjective="amazing" dishes={dishObjects}/>
-            <Footer year={new Date().getFullYear()}/>
-          </div>
-        </React.Fragment>
-      );
+    return (
+      <RegularComponent />
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -4,16 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-function App2() {
-  return (
-    <h1>This is the content from App 2!</h1>
-  )
-}
-
 ReactDOM.render(
-  <React.Fragment>
-    <App authorized={false}/>
-    <App2 />
-  </React.Fragment>,
+    <App authorized={true}/>,
   document.getElementById('root')
 );
