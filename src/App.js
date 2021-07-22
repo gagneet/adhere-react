17ec5a8640474dbdbd974550ad3cb6ed0e0b02ea
Gagneet Singh <gagneet@gmail.com>
2021-07-22 14:01:29 +1000

    Added routing to pages and using the /abc URL to go over to the desired page, no linking as yet

--- src/App.js
+++ src/App.js
@@ -1,40 +1,24 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import {
+  Home,
+  About,
+  Events,
+  Contact
+} from './pages.js';
 
 // https://api.github.com/users/gagneet
 
-function App({login}) {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    if (!login) return;
-    setLoading(true);
-
-    fetch(`https://api.github.com/users/${login}`)
-      .then((response) => response.json())
-      .then(setData)
-      .then(() => setLoading(false))
-      .catch(setError);
-  }, [login]);
-
-  if (loading) return <h1>Loading...</h1>;
-  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
-  if (!data) return null;
-
+function App() {
   return (
     <div>
-      <h1>{data.name}</h1>
-      <h2>{data.company}</h2>
-      <h3>{data.blog}</h3>
-      <h3>{data.id}</h3>
-      <h3>{data.created_at}</h3>
-      <p>
-        {data.location} <br />
-        {data.bio}
-      </p>
-      <img alt={data.login} src={data.avatar_url} height="200" />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
